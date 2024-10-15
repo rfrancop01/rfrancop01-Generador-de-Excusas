ded378500a3479ec8c83722da90f43059181c031
Ricardo Franco Pérez <rfrancop01@gmail.com>
2024-10-15 12:05:55 +0200

    app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
+
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Mi perro", "Mi vecino", "El gato", "El dinosaurio"];
+  let action = ["se comió", "quemó", "rompió", "pisó"];
+  let what = ["los deberes", "el teléfono", "el ordenador", "la moto"];
+  let when = ["ayer", "antes de dormir", "durante el vuelo", "cuando llovía"];
+
+  let excse1 = Math.floor(Math.random() * who.length);
+  let excse2 = Math.floor(Math.random() * action.length);
+  let excse3 = Math.floor(Math.random() * what.length);
+  let excse4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector(".excuse").innerHTML =
+    who[excse1] +
+    " " +
+    action[excse2] +
+    " " +
+    what[excse3] +
+    " " +
+    when[excse4];
 };
